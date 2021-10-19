f9039a684c1ec160cf4f1417bcef64f8a83c503b
rubelsarker0 <rubelsarker0@outlook.com>
2021-10-19 16:20:21 +0100

    small-text-format

--- src/components/Home/HomeServices/HomeServices.js
+++ src/components/Home/HomeServices/HomeServices.js
@@ -8,7 +8,7 @@ const HomeServices = ({ services }) => {
 			<Container>
 				<div className="text-center">
 					<h1 className="regular-text-color">Our Top Most Priority Services</h1>
-					<p className="text-secondary w-75 mx-auto">
+					<p className="text-secondary text-format  w-75 mx-auto">
 						A responsive Emergency Department and Minor Emergency Care Unit
 						treats more than 50,000 patients annually. In recent years, a robust
 						array of online services and portals have been.
